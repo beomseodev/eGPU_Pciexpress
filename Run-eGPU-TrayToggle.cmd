01e219a342702c9c8fa3d60d79cbd2aa915f04d4
beomseodev <beomseodev@gmail.com>
2026-06-01 15:05:09 +0900

    chore: ignore build outputs

--- Run-eGPU-TrayToggle.cmd
+++ Run-eGPU-TrayToggle.cmd
@@ -2,7 +2,14 @@
 setlocal
 
 set "SCRIPT_DIR=%~dp0"
-set "APP=%SCRIPT_DIR%EGPU.TrayToggle\bin\Release\net8.0-windows\EGPU.TrayToggle.exe"
+set "APP=%SCRIPT_DIR%dist\eGPU-TrayToggle\EGPU.TrayToggle.exe"
+
+if exist "%APP%" (
+    start "" "%APP%"
+    exit /b 0
+)
+
+set "APP=%SCRIPT_DIR%EGPU.TrayToggle\bin\Release\net8.0-windows\win-x64\EGPU.TrayToggle.exe"
 
 if exist "%APP%" (
     start "" "%APP%"
@@ -11,6 +18,5 @@ if exist "%APP%" (
 
 echo EGPU.TrayToggle.exe was not found.
 echo Build it first with:
-echo dotnet build "%SCRIPT_DIR%EGPU.TrayToggle\EGPU.TrayToggle.csproj" -c Release
+echo dotnet publish "%SCRIPT_DIR%EGPU.TrayToggle\EGPU.TrayToggle.csproj" -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o "%SCRIPT_DIR%dist\eGPU-TrayToggle"
 exit /b 1
-
